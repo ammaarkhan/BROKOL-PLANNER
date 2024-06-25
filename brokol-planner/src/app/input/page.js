4bb73b83ef22a0d63e93b3397f6904eecdbc2606
ammaarkhan <ammaarkhan03@gmail.com>
2024-06-24 18:50:50 -0700

    move sign in to home page

--- brokol-planner/src/app/input/page.js
+++ brokol-planner/src/app/input/page.js
@@ -4,9 +4,7 @@ export default function Input() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl mb-4">Meal Planner</h1>
-      <Layout/>
+      <Layout />
     </main>
-    
   );
 }
-
