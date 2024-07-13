c523803229d0933a96c84fd6a42a82fea101840c
Ammaar Khan <98288912+ammaarkhan@users.noreply.github.com>
2024-07-12 23:19:57 -0700

    Save Input and Analytics (#11)
    * Firebase analytics implementation (#9)
    
    * implemented analytics for all pages
    
    * added log event for removing recipes
    
    * revert useAnalytics without debounce, added logevents for saving mealplan and more mealplan and generate recipe
    
    * added reference for analytics in inputpage
    
    * made the generate recipe log event anonymous function
    
    * remove repetitive logevent for generating recipes
    
    * change useAnalytics to logPage
    
    * removed unnecessary import lines
    
    * change hook name from logPage to useLogPage
    
    * remove console log comment
    
    ---------
    
    Co-authored-by: ammaarkhan <ammaarkhan03@gmail.com>
    
    * Internal main (#10)
    
    * remove analytics from internal-main
    
    * implemented storing and retrieving user input data (#8)
    
    * implemented storing and retrieving user input data
    
    * fixed not querying gpt
    
    * simplified the params for formData
    
    * added a comment
    
    * minor clean up
    
    ---------
    
    Co-authored-by: ammaarkhan <ammaarkhan03@gmail.com>
    
    ---------
    
    Co-authored-by: Shin Tan <70144466+ufotan@users.noreply.github.com>
    
    * update to include analytics
    
    * edit home page test
    
    * revert home page test
    
    * minor change for new deployment
    
    * revert change to deploy again
    
    * update logPage to page title
    
    * remove logpage on sign in page
    
    ---------
    
    Co-authored-by: Shin Tan <70144466+ufotan@users.noreply.github.com>

---
+++ brokol-planner/src/app/hooks/useLogPage.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { logEvent } from "firebase/analytics";
+import { analytics } from "../../config/firebase";
+
+const useLogPage = () => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (analytics) {
+      logEvent(analytics, "page_view", { page_title: pathname });
+    }
+  }, [pathname]); // Only re-run the effect if pathname changes
+};
+
+export default useLogPage;
