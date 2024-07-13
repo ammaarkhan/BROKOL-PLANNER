a7995da297276bd0cded1caac5267b71d19dbfbe
ammaarkhan <ammaarkhan03@gmail.com>
2024-07-12 18:01:01 -0700

    remove console log comment

--- brokol-planner/src/app/hooks/useLogPage.js
+++ brokol-planner/src/app/hooks/useLogPage.js
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 
-import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
-import { logEvent } from 'firebase/analytics';
-import { analytics } from '../../config/firebase';
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { logEvent } from "firebase/analytics";
+import { analytics } from "../../config/firebase";
 
-  const useLogPage = () => {
-    const pathname = usePathname();
-  
-    useEffect(() => {
-      if (analytics) {
-        logEvent(analytics, 'page_view', { page_path: pathname });
-        //console.log('Page view logged:', pathname);
-      }
-    }, [pathname]); // Only re-run the effect if pathname changes
-  };
+const useLogPage = () => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (analytics) {
+      logEvent(analytics, "page_view", { page_path: pathname });
+    }
+  }, [pathname]); // Only re-run the effect if pathname changes
+};
 
 export default useLogPage;
