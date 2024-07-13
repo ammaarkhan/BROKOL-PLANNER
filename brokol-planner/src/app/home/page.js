5a7b94e0eef2fa0e53936b99845584055065976c
ammaarkhan <ammaarkhan03@gmail.com>
2024-07-12 21:10:28 -0700

    edit home page test

--- brokol-planner/src/app/home/page.js
+++ brokol-planner/src/app/home/page.js
@@ -3,17 +3,18 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import withAuth from "../firebase/withAuth";
-import useLogPage from '../hooks/useLogPage';
+import useLogPage from "../hooks/useLogPage";
 
 function Home() {
-
   useLogPage();
 
   const router = useRouter();
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-center flex-1 py-5">
-        <h1 className="text-4xl font-bold text-center mb-10">Homepage</h1>
+        <h1 className="text-4xl font-bold text-center mb-10">
+          Homepage Analytics main
+        </h1>
         <div className="flex gap-8">
           <button
             className="w-48 h-32 font-bold bg-white border-2 border-black text-black text-xl rounded-lg hover:bg-gray-200"
