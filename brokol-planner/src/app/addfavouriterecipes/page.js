57c81007332a3f9da804604d57f19fb3b39ea465
ufotan <ufotanshin0508@gmail.com>
2024-07-16 18:17:55 -0700

    remove some imports for addfavrecipes

--- brokol-planner/src/app/addfavouriterecipes/page.js
+++ brokol-planner/src/app/addfavouriterecipes/page.js
@@ -1,9 +1,7 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
 import withAuth from "../firebase/withAuth";
-import useLogPage from "../hooks/useLogPage";
 
 function AddFavRecipes({searchParams}) {
   return (
